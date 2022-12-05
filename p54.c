1b801b51c3a4983c81bc652e6be3c9df800b93ac
ABHI-SOLANKI <117363808+ABHI-SOLANKI@users.noreply.github.com>
2022-12-05 18:09:28 +0530

    Add files via upload

---
+++ p54.c
@@ -0,0 +1,17 @@
+//WAP to calculate sum of 5 numbers using arrays.
+
+#include<stdio.h>
+int main()
+{
+    int a[5],i,sum=0;
+    printf("Enter 5 Numbers :");
+    for(i=0;i<=4;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<=4;i++)
+    {
+        sum=sum+a[i];
+    }
+    printf("sum is :%d",sum);
+}
