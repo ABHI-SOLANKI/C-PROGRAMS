3ee365dd05308e02f3659f3e33eb18b1ca189a7c
ABHI-SOLANKI <117363808+ABHI-SOLANKI@users.noreply.github.com>
2022-12-06 15:12:22 +0530

    Add files via upload

---
+++ p55.c
@@ -0,0 +1,12 @@
+//  [ PRINTING STRING ].
+
+#include<stdio.h>
+int main()
+{
+    char str[10]={'A','B','H','I',};
+    int i;
+    for(i=0;i<=3;i++)
+    {
+        printf("%c",str[i]);
+    }
+}
---
+++ p56.c
@@ -0,0 +1,13 @@
+//  [ PRINTING SRING USING NULL CHARECTER ].
+
+#include<stdio.h>
+int main()
+{
+    char str[10]={'A','B','H','I',};
+    int i;
+    for(i=0;str[i];i++)
+    {
+        printf("%c",str[i]);
+    }
+
+}
