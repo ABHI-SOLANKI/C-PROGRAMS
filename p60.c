1f1c57542240ce76ae0b5eba8a1461fc0cccac0d
ABHI-SOLANKI <117363808+ABHI-SOLANKI@users.noreply.github.com>
2022-12-09 16:56:22 +0530

    Add files via upload

---
+++ p60.c
@@ -0,0 +1,14 @@
+//Write a program to convert temperature from degree centigrade to fahrenheit.
+
+#include<stdio.h>
+int main()
+{
+    float celsius,fahrenheih;
+    printf("Enter Temp in celsius :");
+    scanf("%f",&celsius);
+
+    fahrenheih=(1.8*celsius)+32;
+    printf("\n Temperature in fahrenheit :%f",fahrenheih);
+
+    return(0);
+}
---
+++ p61.c
@@ -0,0 +1,18 @@
+//WAP which print fibonacci series up to n numbers.
+
+#include<stdio.h>
+int main()
+{
+    int n,i,t1=0,t2=1,nextTerm;
+    printf("Enter A Number Of Intger :-");
+    scanf("%d",&n);
+
+    printf("Fibonacci series :-");
+    for(i=1;i<=n;++i)
+    {
+        printf("%d",t1);
+        nextTerm=t1+t2;
+        t1=t2;
+        t2=nextTerm;
+    }
+}
