c8fe448c8fc56d8623ab52300ac1a8be4abd92fc
ABHI-SOLANKI <117363808+ABHI-SOLANKI@users.noreply.github.com>
2022-12-15 12:33:44 +0530

    Add files via upload

---
+++ p64.c
@@ -0,0 +1,20 @@
+//WAP to read 2 numbers from keybrod and find oot the maximum value out of them.
+
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter A Number 1:-");
+    scanf("%d",&a);
+    printf("Enter A Number 2:-");
+    scanf("%d",&b);
+
+    if(a<b==0)
+    {
+        printf("%d Maximum Number :-",a);
+    }
+    else
+    {
+        printf("%d Maximum Number :-",b);
+    }
+}
