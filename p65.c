af0dfc5ae11e2863540accb6ae8ee8b65b402101
ABHI-SOLANKI <117363808+ABHI-SOLANKI@users.noreply.github.com>
2022-12-16 20:56:34 +0530

    Add files via upload

---
+++ p65.c
@@ -0,0 +1,28 @@
+//WAP to print the following pattern.
+/*
+    *****
+    *****
+    **0**
+    *****
+*/
+
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=5;j++)
+        {
+            if(i==3 && j==3)
+            {
+                printf("0");
+            }
+            else
+            {
+                printf("*");
+            }
+        }
+        printf("\n");
+    }
+}
