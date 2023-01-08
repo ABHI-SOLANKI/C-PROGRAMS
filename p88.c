93697b12683abdeecc476d87885846e82fb5279a
Abhi Solanki <117363808+ABHI-SOLANKI@users.noreply.github.com>
2023-01-08 13:35:49 +0530

    Add files via upload

---
+++ p88.c
@@ -0,0 +1,13 @@
+//WAP to print 1 to 10 using do while loop.
+
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    do
+    {
+        printf("%d",i);
+        printf("\n");
+        i++;    
+    } while (i<=10);
+}
