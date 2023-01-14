e9f5026380b031b02aafe4f7c114bcb5ed8d7ab5
Abhi Solanki <117363808+ABHI-SOLANKI@users.noreply.github.com>
2023-01-14 19:21:54 +0530

    Add files via upload

---
+++ p94.c
@@ -0,0 +1,13 @@
+//WAP to print Z to A using do while loop amd ASCII CODE.
+
+#include<stdio.h>
+int main()
+{
+    int i=90;
+    do
+    {
+        printf("\t %c",i);
+        i--;
+    } while(i>=65);
+    
+}
