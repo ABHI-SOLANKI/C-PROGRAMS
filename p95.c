a8cc82c9df0fe273ef11723f7e0142fd7db4ffb6
Abhi Solanki <117363808+ABHI-SOLANKI@users.noreply.github.com>
2023-01-15 11:41:42 +0530

    Add files via upload

---
+++ p95.c
@@ -0,0 +1,12 @@
+//WAP to print z to a using do while loop and ASCII CODE.
+
+#include<stdio.h>
+int main()
+{
+    int i=122;
+    do
+    {
+        printf("\t%c",i);
+        i--;
+    } while (i>=97);
+}
