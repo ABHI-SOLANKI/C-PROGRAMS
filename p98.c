d3479dc85d906e70334b4d677da113937fa5126a
Abhi Solanki <117363808+ABHI-SOLANKI@users.noreply.github.com>
2023-01-18 19:23:37 +0530

    Add files via upload

---
+++ p98.c
@@ -0,0 +1,19 @@
+//WAP which print claculate the division of two value using union,
+
+#include<stdio.h>
+union div
+{
+    int a,b;
+}s1;
+int main()
+{
+    int ans;
+    printf("Enter A Number 1:-");
+    scanf("%d",&s1.a);
+    printf("Enter A Number 2:-");
+    scanf("%d",&s1.b);
+
+    ans=s1.a/s1.b;
+    printf("Ans Is =%d",ans);
+
+}
