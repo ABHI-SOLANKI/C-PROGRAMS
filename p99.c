20a97ee60653eb4f4a5df763ff84dbab50add614
Abhi Solanki <117363808+ABHI-SOLANKI@users.noreply.github.com>
2023-01-19 14:31:30 +0530

    Add files via upload

---
+++ p99.c
@@ -0,0 +1,17 @@
+//WAP to entered 5 numbers and find the maximum number using arry.
+
+#include<stdio.h>
+int main()
+{
+    int a[5],i,max; 
+    printf("Enter A Numebr:-");
+    for(i=1;i<=5;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    if(max>=a[i])
+    {
+        printf("Maximum Number %d",max);
+    }
+        
+}
