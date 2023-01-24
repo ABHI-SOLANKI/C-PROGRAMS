09307ea04400d0a8510488726f4d94fae7ae2499
Abhi Solanki <117363808+ABHI-SOLANKI@users.noreply.github.com>
2023-01-24 20:43:21 +0530

    Add files via upload

--- p45.c
+++ p45.c
@@ -1,19 +1,23 @@
-//write 2 function -one to print "Hello" & second to print "Good Bye".
+//WAP to  take single charecter from the user and check it's a vowel charecter or not.
 
 #include<stdio.h>
-void printHello();
-void printGoodBye();
+int mani()
+{
+    char ch;
+    printf("Enter A Charecter :-");
+    scanf("%c",&ch);
 
-int main()
-{
-    printHello();
-    printGoodBye();
-}
-void printHello()
-{
-    printf("Hello\n");
-}
-void printGoodBye()
-{
-    printf("Good Bye\n");
+    if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+    {
+        printf("It's A Vowel");
+    }
+    else if (ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
+    {
+        printf("It's A Vowel");
+    }
+    else
+    {
+        printf("It's Not A Vowel");
+    }
+    
 }
