98ebd41f047c5f1e85b803a6a1aaaf867d6cf433
Abhi Solanki <117363808+ABHI-SOLANKI@users.noreply.github.com>
2023-01-27 18:44:45 +0530

    Add files via upload

---
+++ Sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,ans;
+    printf("Enter A Number 1:-");
+    scanf("%d",&a);
+
+    printf("Enter A Number 2:-");
+    scanf("%d",&b);
+
+    ans=a+b;
+    printf("Sum is =%d",ans);
+}
